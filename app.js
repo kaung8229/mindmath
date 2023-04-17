b1f070a2a5d2b99586b20c5fe359c17ab7717877
Kaung Myat <kaung8229@gmail.com>
2023-04-17 21:47:43 +0630

    mind math 17-4-2023

--- app.js
+++ app.js
@@ -27,6 +27,14 @@ var answernumber = document.getElementById('answernumber');
 var checkbtn = document.querySelector('.checkbtn');
 var restartbtn = document.querySelector('.restartbtn');
 
+// for level
+var easy = document.getElementById("easy");
+var norm = document.getElementById("norm");
+var hard = document.getElementById("hard");
+var impo = document.getElementById("impo");
+
+var minus = document.getElementById('minus');
+
 
 startbtn.addEventListener('click',function(e){
     // e.preventDefault();
@@ -51,6 +59,8 @@ restartbtn.addEventListener('click',function(){
 var rannum = 0;
 var rightans = 0;
 
+var levels = 0;
+
 // const synth = window.speechSynthesis
 // let textt = "Hey there";
 // let utterthis = new SpeechSynthesisUtterance(textt);
@@ -63,6 +73,24 @@ function progresscheck(){
     // console.log(counts.value);
     // console.log(txt.checked);
     // console.log(voc.checked);
+
+    if(easy.checked){
+        levels = 1300;
+    }else if(norm.checked){
+        levels = 1100;
+    }else if(hard.checked){
+        levels = 800;
+    }else if(impo.checked){
+        levels = 500;
+    }
+
+
+
+    var numdig = 1;
+    for(var n = 1; n < (+digits.value); n++){
+        numdig *= 10;
+    }
+    // console.log(numdig);
     
     // randomnumber();
 
@@ -77,15 +105,19 @@ function progresscheck(){
             if(c <= ((+counts.value)-1)){
                 setTimeout(function(){
                     // console.log("Hello");
-                    randomnumber()
+                    if(minus.checked){
+                        randomnumber(((numdig * -10) + 1),(numdig * 10) -1,numdig);
+                    }else{
+                        randomnumber(numdig,(numdig * 10) -1,numdig);
+                    }
                     numui.innerHTML = rannum;
-                },(1200 * (c + 2)))
+                },(levels * (c + 2)))
             }else{
                 setTimeout(function(){
                     // console.log("finished");
                     closeshownum();
                     showanswerbox();
-                },(1200 * ((+counts.value)+2)))
+                },(levels * ((+counts.value)+2)))
             }
         }
     }
@@ -103,7 +135,11 @@ function progresscheck(){
             if(c <= ((+counts.value)-1)){
                 setTimeout(function(){
                     // console.log("Hello");
-                    randomnumber();
+                    if(minus.checked){
+                        randomnumber((numdig * -1),(numdig * 10) -1,numdig);
+                    }else{
+                        randomnumber(numdig,(numdig * 10) -1,numdig);
+                    }
                     speaknumbervoice();
                 },(1500 * (c + 2)))
             }else{
@@ -119,30 +155,54 @@ function progresscheck(){
 
 }
 
-function randomnumber(){
+var once = false;
+var mi = 0;
+
+function randomnumber(min,max,numdig){
     // console.log(digits.value);
 
-    var numdig = 1;
-    for(var n=1; n <= digits.value; n++){
-        numdig *= 10;
-    }
-    // console.log(numdig);
+    // console.log(min);
+    // console.log(max);
 
-    rannum = Math.round((Math.random() * numdig) + 1);
+    rannum = Math.floor(min + (Math.random() * (max - min)));
+    // console.log(rannum);
+
+    if(minus.checked){
+        // console.log(numdig);
+        if(rannum > -numdig && rannum < numdig){
+            rannum = (rannum * 10) + Math.floor(Math.random() * 10);
+        }else if(rannum == 0){
+            rannum = rannum + numdig;
+        }
+        // console.log(rannum);
+        if(mi < 1){
+            // console.log("checked");
+            rannum = rannum * checkfirstminus(rannum);
+            mi++;
+        }
+    }
     // console.log(rannum);
 
     rightans += rannum;
 
-    
-
 }
 
-function speaknumbervoice(){
+function checkfirstminus(nn){
+    if(!once && Math.sign(nn) == -1){
+        once = true;
+        return -1;
+    }else{
+        return 1;
+    }
+}
 
-    const speaker = window.speechSynthesis;
+const speaker = window.speechSynthesis;
+
+function speaknumbervoice(){
+    
     const utternum = new SpeechSynthesisUtterance(rannum);
 
-    utternum.rate = 2;
+    utternum.rate = 1.5;
     utternum.lang = "en-US";
 
     speaker.speak(utternum);
@@ -158,8 +218,11 @@ function showreadytext(){
             if(r < 2){
                 // console.log(readytext[r++]);
                 numui.innerHTML = readytext[r++];
+                numui.style.animationName = "numani";
+                numui.style.animationDuration = `${(levels/1000)}s`;
+                numui.style.animationDelay = "0.01s";
             }
-        },(1200 * (i + 1)));
+        },(levels * (i + 1)));
     }
 
 }
@@ -245,6 +308,9 @@ function restarter(){
     rightans = 0;
 
 }
+
+
+
 
 
 
